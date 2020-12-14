bf158931bdfcb4a88eabdddc2973d6f6f2fabdfb
Churchjg <churchjg283@gmail.com>
2020-12-14 16:41:17 -0500

    changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Homepage from './Components/Homepage/Homepage';
+import Gallery from './Components/Gallery/Gallery'
+
+export default function App() {
+
+  return (
+    <Homepage />
+  )
+}
